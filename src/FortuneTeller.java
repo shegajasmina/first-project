3f6078478c83395e4dd6c0e4272280edd0214ba9
Jasmina Shega <shegajasmina@yahoo.com>
2017-08-27 18:26:34 -0400

    first push

---
+++ src/FortuneTeller.java
@@ -0,0 +1,61 @@
+	
+
+import java.util.Scanner;
+
+public class FortuneTeller  {
+
+
+	public static void main(String[] args) {
+		
+		Scanner scanner1 = new Scanner(System.in);
+		
+		System.out.println("Enter Your First Name: ");
+		scanner1.nextLine();
+		
+		System.out.println("Enter Your Last Name: ");
+		scanner1.nextLine();
+		
+		System.out.println("Enter Your Age: ");
+		int age = scanner1.nextInt();
+		
+		System.out.println("Enter Your Favortie ROYGBIV Color: " + "Enter (Help) If You Do Not Know What ROYGBIV Is: ");
+		 scanner1.nextLine();
+		 
+		System.out.println("Enter (Help) If You Do Not Know What ROYGBIV Is: " );	  
+		scanner1.nextLine();
+		
+		System.out.println("Enter Your Number of Siblings: ");
+		 int numberOfSiblings = scanner1.nextInt(); 
+		 
+		System.out.println("Enter Your Birth Month: ");
+		 int birthMonth= scanner1.nextInt(); 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+		 
+	}}
+		 
+	
+
+		
+		
+		
+	
+              
+
+		
+		
+        		
+		
+		
+		
+		
+		
+	
+
